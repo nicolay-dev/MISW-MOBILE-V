8fb4eeaac3b2597f2084c3d07c613af8b6e61a88
PinedaJuan98 <jc.pineda@uniandes.edu.co>
2022-11-03 14:25:38 -0500

    Init HU02

--- app/src/main/java/com/example/vinylteam8/network/NetworkServiceAdapter.kt
+++ app/src/main/java/com/example/vinylteam8/network/NetworkServiceAdapter.kt
@@ -2,13 +2,13 @@ package com.example.vinylteam8.network
 
 import android.content.Context
 
-class NetworkService constructor(context: Context){
+class NetworkServiceAdapter constructor(context: Context){
     companion object{
         const val BASE_URL= "https://backend-vynils-tsdl.herokuapp.com/"
-        var instance: NetworkService? = null
+        var instance: NetworkServiceAdapter? = null
         fun getInstance(context: Context) =
             instance ?: synchronized(this) {
-                instance ?: NetworkService(context).also {
+                instance ?: NetworkServiceAdapter(context).also {
                     instance = it
                 }
             }
---
+++ app/src/main/java/com/example/vinylteam8/viewmodels/PerformerViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.vinylteam8.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.*
+import com.example.vinylteam8.models.Performer
+import com.example.vinylteam8.network.NetworkServiceAdapter
+import com.example.vinylteam8.repositories.PerformerRepository
+
+class PerformerViewModel {
+}
