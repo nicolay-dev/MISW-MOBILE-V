0575ff947bb4b39c9710bda294c40f2f454be427
leslysharyn <leslysharyncj@gmail.com>
2022-11-05 09:25:53 -0500

    HU01 Implementación de Request Albumes (#46)
    * First structure for the mvvm
    
    * Class
    
    * Getting and showing list of albums
    
    * Test for Albums.
    
    * Images in the album List

--- app/src/main/java/com/example/vinylteam8/ui/MainActivity.kt
+++ app/src/main/java/com/example/vinylteam8/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.vinylteam8
+package com.example.vinylteam8.ui
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,16 +7,17 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.vinylteam8.databinding.ActivityNavigationBinding
+import com.example.vinylteam8.R
+import com.example.vinylteam8.databinding.ActivityMainBinding
 
-class NavigationActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityNavigationBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityNavigationBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
---
+++ app/src/main/java/com/example/vinylteam8/ui/adapters/AlbumAdapter.kt
@@ -0,0 +1,66 @@
+package com.example.vinylteam8.ui.adapters
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.vinylteam8.R
+import com.example.vinylteam8.databinding.FragmentAlbumListBinding
+import com.example.vinylteam8.models.Album
+import com.squareup.picasso.Picasso
+
+
+class AlbumsAdapter : RecyclerView.Adapter<AlbumsAdapter.AlbumViewHolder>(){
+
+    var albums :List<Album> = emptyList()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumViewHolder {
+        val withDataBinding: FragmentAlbumListBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            AlbumViewHolder.LAYOUT,
+            parent,
+            false)
+        return AlbumViewHolder(withDataBinding)
+    }
+
+    override fun onBindViewHolder(holder: AlbumViewHolder, position: Int) {
+        holder.viewDataBinding.also {
+            it.album = albums[position]
+        }
+
+        //Picasso
+        Picasso.get()
+            .load(albums[position].cover) // internet path
+            .into(holder.viewDataBinding.imagelist);  //container
+        /*
+        holder.viewDataBinding.root.setOnClickListener {
+            val action = FragmentAlbumDirections.actionAlbumFragmentToCommentFragment(albums[position].albumId)
+            // Navigate using that action
+            holder.viewDataBinding.root.findNavController().navigate(action)
+        }
+
+         */
+    }
+
+    override fun getItemCount(): Int {
+        return albums.size
+    }
+
+
+    class AlbumViewHolder(val viewDataBinding: FragmentAlbumListBinding) :
+        RecyclerView.ViewHolder(viewDataBinding.root) {
+        companion object {
+            @LayoutRes
+            val LAYOUT = R.layout.fragment_album_list
+        }
+    }
+
+
+}
